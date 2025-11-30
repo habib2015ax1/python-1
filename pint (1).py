9f30a0cedd13585906f86d28641d80c90ebef9ab
mhabibwani@gmail.com <Habib1@#$>
2025-11-30 11:37:47 +0400

    hello save

---
+++ pint (1).py
@@ -0,0 +1,7 @@
+print ("hello user")
+name=input("enter you name")
+print("Wellcome",name)
+print ("how can i help buti can only help in instaling pyton ")
+print ("step 1 here a re links to the pyton download page https://www.python.org/downloads/")
+habib
+h
---
+++ v.py
@@ -0,0 +1,8 @@
+name="habib"
+age=10
+pasword="habib"
+email_id="mhabibwani@gmail.com"
+print('my name is',name)
+print('my age  is',age)
+print('my password is',pasword)
+print('my email_id is',email_id)
