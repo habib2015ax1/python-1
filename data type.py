d8bc45749b42dfe7f9e7d0746c186c8d97d420da
mhabibwani@gmail.com <Habib1@#$>
2025-12-07 11:18:39 +0400

    data type

---
+++ data type.py
@@ -0,0 +1,8 @@
+age=10
+print("type of age",type(age))
+name="habib"
+print("type of name",type(name))
+hight=135.0
+print("type of hight",type(hight))
+flag=True 
+print ("type of flag",type(flag))
