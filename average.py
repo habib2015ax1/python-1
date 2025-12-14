f6aa068c08c8988aad9cb1ee528b50a709324cc9
mhabibwani@gmail.com <Habib1@#$>
2025-12-14 11:33:48 +0400

    chip bag

--- average.py
+++ average.py
@@ -1,8 +1,8 @@
-tree1 = int(input("enter the hight of tree"))
-tree2 = int(input("enter the hight of tree"))
-tree3 = int(input("enter the hight of tree"))
-tree4 = int(input("enter the hight of tree"))
-tree5 = int(input("enter the hight of tree"))
-sum = tree1 + tree2 + tree3 + tree4 + tree5
+chipbag1 = int(input("enter the amount of chipbag"))
+chipbag2 = int(input("enter the amount of chipbag"))
+chipbag3 = int(input("enter the amount of chipbag"))
+chipbag4 = int(input("enter the amount of chipbag"))
+chipbag5 = int(input("enter the amount of chipbag"))
+sum = chipbag1 + chipbag2 + chipbag3 + chipbag4 + chipbag5
 avg = sum / 5 
-print ("avg of tree is",avg)
+print ("avg of chipbag is",avg)
