f5fc71dfd96ef7d99fe23157f6e15407b4338526
mhabibwani@gmail.com <Habib1@#$>
2025-12-14 11:25:36 +0400

    avg is usefull

---
+++ average.py
@@ -0,0 +1,8 @@
+tree1 = int(input("enter the hight of tree"))
+tree2 = int(input("enter the hight of tree"))
+tree3 = int(input("enter the hight of tree"))
+tree4 = int(input("enter the hight of tree"))
+tree5 = int(input("enter the hight of tree"))
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+avg = sum / 5 
+print ("avg of tree is",avg)
