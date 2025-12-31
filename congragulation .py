4927bbb77d4662a59e842fc034775fb51bb699cc
mhabibwani@gmail.com <Habib1@#$>
2025-12-31 18:29:26 +0400

    congratlation

---
+++ congragulation .py
@@ -0,0 +1,8 @@
+input ("Congratulation")
+correct_word= "Congratulation" 
+user_input= input (f"please type`{correct_word}`: ")
+if user_input == correct_word:
+    print("CONGRATULATION")
+else:
+    print("Congratulation")
+ 
